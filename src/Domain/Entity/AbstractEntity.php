3270fb85f7990ad708016b71227961bae5e41fcb
ludovicjj <jahanlud@gmail.com>
2019-03-21 17:48:43 +0100

    Add properties createdAt and updatedAt in AbstractEntity

--- src/Domain/Entity/AbstractEntity.php
+++ src/Domain/Entity/AbstractEntity.php
@@ -13,12 +13,24 @@ abstract class AbstractEntity
     protected $id;
 
     /**
+     * @var \DateTime
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime|null
+     */
+    protected $updatedAt;
+
+    /**
      * AbstractEntity constructor.
      * @throws \Exception
      */
     public function __construct()
     {
         $this->id = Uuid::uuid4()->toString();
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = null;
     }
 
     /**
@@ -27,5 +39,21 @@ abstract class AbstractEntity
     public function getId()
     {
         return is_object($this->id) ? $this->id->toString() : $this->id;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getUpdatedAt(): ?\DateTime
+    {
+        return $this->updatedAt;
     }
 }
