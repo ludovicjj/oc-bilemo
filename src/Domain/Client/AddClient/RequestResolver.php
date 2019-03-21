933764360fef576bd8c9f9bb86a2786b81a64d17
ludovicjj <jahanlud@gmail.com>
2019-03-21 19:27:32 +0100

    Hydrate AddClientInput with content from request

--- src/Domain/Client/AddClient/RequestResolver.php
+++ src/Domain/Client/AddClient/RequestResolver.php
@@ -19,6 +19,11 @@ class RequestResolver
 
     public function resolve(Request $request)
     {
-
+        /** @var AddClientInput $input */
+        $input = $this->serializer->deserialize(
+            $request->getContent(),
+            AddClientInput::class,
+            'json'
+        );
     }
 }
