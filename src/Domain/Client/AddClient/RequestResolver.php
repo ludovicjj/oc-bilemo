2fc5a97cac1a5c8ede62e689885e14c38fef6ad6
ludovicjj <jahanlud@gmail.com>
2019-03-21 18:42:52 +0100

    Init RequestResolver with method resolve

---
+++ src/Domain/Client/AddClient/RequestResolver.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Domain\Client\AddClient;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class RequestResolver
+{
+    public function resolve(Request $request)
+    {
+
+    }
+}
