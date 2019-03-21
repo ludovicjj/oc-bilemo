5cdec97c3ba8f2dcfbaf34cf343f735dabde0115
ludovicjj <jahanlud@gmail.com>
2019-03-21 23:42:53 +0100

    Update ExceptionSubscriber

--- src/Domain/Common/ExceptionSubscriber.php
+++ src/Domain/Common/ExceptionSubscriber.php
@@ -2,11 +2,13 @@
 
 namespace App\Domain\Common;
 
+use App\Domain\Common\Exceptions\ProcessorErrorsHttp;
 use App\Responders\JsonResponder;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 use App\Domain\Common\Exceptions\ValidatorException;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ExceptionSubscriber implements EventSubscriberInterface
 {
@@ -23,9 +25,15 @@ class ExceptionSubscriber implements EventSubscriberInterface
             case ValidatorException::class:
                 $this->processValidatorException($event);
                 break;
+            case ProcessorErrorsHttp::class:
+                $this->processHttpException($event);
+                break;
         }
     }
 
+    /**
+     * @param GetResponseForExceptionEvent $event
+     */
     public function processValidatorException(GetResponseForExceptionEvent $event)
     {
         /** @var ValidatorException $exception */
@@ -33,6 +41,21 @@ class ExceptionSubscriber implements EventSubscriberInterface
         $event->setResponse(
             JsonResponder::response(
                 json_encode($exception->getErrors()),
+                $exception->getStatusCode()
+            )
+        );
+    }
+
+    /**
+     * @param GetResponseForExceptionEvent $event
+     */
+    private function processHttpException(GetResponseForExceptionEvent $event)
+    {
+        /** @var HttpException $exception */
+        $exception = $event->getException();
+        $event->setResponse(
+            JsonResponder::response(
+                json_encode(['message' => $exception->getMessage()]),
                 $exception->getStatusCode()
             )
         );
