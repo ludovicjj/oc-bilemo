376461f602228a74c9d0ed672753ed80d6c88430
ludovicjj <jahanlud@gmail.com>
2019-03-21 17:34:52 +0100

    Define id type Uuid in AbstractEntity

--- src/Domain/Entity/AbstractEntity.php
+++ src/Domain/Entity/AbstractEntity.php
@@ -2,8 +2,30 @@
 
 namespace App\Domain\Entity;
 
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
 
 abstract class AbstractEntity
 {
+    /**
+     * @var string|UuidInterface
+     */
+    protected $id;
 
+    /**
+     * AbstractEntity constructor.
+     * @throws \Exception
+     */
+    public function __construct()
+    {
+        $this->id = Uuid::uuid4()->toString();
+    }
+
+    /**
+     * @return UuidInterface|string
+     */
+    public function getId()
+    {
+        return is_object($this->id) ? $this->id->toString() : $this->id;
+    }
 }
