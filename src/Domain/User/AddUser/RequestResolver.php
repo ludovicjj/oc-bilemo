9759bf87ec2c0dd0bcc352fd38a37c2b26133755
ludovicjj <jahanlud@gmail.com>
2019-03-21 23:35:20 +0100

    If ClientVoter return false throw new HttpException

--- src/Domain/User/AddUser/RequestResolver.php
+++ src/Domain/User/AddUser/RequestResolver.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Security\Core\Security;
 
 class RequestResolver
 {
@@ -20,20 +21,25 @@ class RequestResolver
     /** @var SerializerInterface */
     protected $serializer;
 
+    protected $security;
+
     /**
      * RequestResolver constructor.
      * @param TokenStorageInterface $tokenStorage
      * @param ValidatorInterface $validator
      * @param SerializerInterface $serializer
+     * @param Security $security
      */
     public function __construct(
         TokenStorageInterface $tokenStorage,
         ValidatorInterface $validator,
-        SerializerInterface $serializer
+        SerializerInterface $serializer,
+        Security $security
     ) {
         $this->tokenStorage = $tokenStorage;
         $this->validator = $validator;
         $this->serializer = $serializer;
+        $this->security = $security;
     }
 
     /**
@@ -41,10 +47,17 @@ class RequestResolver
      * @return AddUserInput
      * @throws \App\Domain\Common\Exceptions\ValidatorException
      */
-    public function resolve(Request $request)
+    public function resolve(Request $request): AddUserInput
     {
         /** @var Client $client */
         $client = $this->tokenStorage->getToken()->getUser();
+        $clientId = $request->attributes->get('client_id');
+
+        if (!$this->security->isGranted('CLIENT_CHECK', $clientId)) {
+            ProcessorErrorsHttp::throwAccessDenied(
+                'Vous n\'êtes pas autorisé à ajouter un utilisateur dans ce catalogue.'
+            );
+        }
 
         /** @var AddUserInput $input */
         $input = $this->serializer->deserialize(
