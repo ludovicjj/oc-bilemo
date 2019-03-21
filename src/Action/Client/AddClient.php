412b6260b64a2f3ff2fe8322c3792b541f78e3c2
ludovicjj <jahanlud@gmail.com>
2019-03-21 19:37:55 +0100

    Update AddClient with RequestResolver

--- src/Action/Client/AddClient.php
+++ src/Action/Client/AddClient.php
@@ -2,17 +2,32 @@
 
 namespace App\Action\Client;
 
+use App\Domain\Client\AddClient\RequestResolver;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AddClient
 {
+    /** @var RequestResolver  */
+    protected $requestResolver;
+
+    /**
+     * AddClient constructor.
+     * @param RequestResolver $requestResolver
+     */
+    public function __construct(
+        RequestResolver $requestResolver
+    )
+    {
+        $this->requestResolver = $requestResolver;
+    }
+
     /**
      * @Route("/api/clients", name="add_client", methods={"POST"})
      * @param Request $request
      */
     public function add(Request $request)
     {
-
+        $this->requestResolver->resolve($request);
     }
 }
