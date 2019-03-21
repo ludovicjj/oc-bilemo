ecbfada7b2436cc5f2cd716bf33918bb9ea15ce6
ludovicjj <jahanlud@gmail.com>
2019-03-21 19:14:19 +0100

    Recipe symfony/serializer

--- src/Domain/Client/AddClient/RequestResolver.php
+++ src/Domain/Client/AddClient/RequestResolver.php
@@ -3,9 +3,20 @@
 namespace App\Domain\Client\AddClient;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class RequestResolver
 {
+    /** @var SerializerInterface */
+    protected $serializer;
+
+    public function __construct(
+        SerializerInterface $serializer
+    )
+    {
+        $this->serializer = $serializer;
+    }
+
     public function resolve(Request $request)
     {
 
