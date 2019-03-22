b0879402a7ad55667df99a25bd4c1a6d8aa06a15
ludovicjj <jahanlud@gmail.com>
2019-03-22 05:22:43 +0100

    Expose propertie name for groups=show_phone in Maker Entity

--- src/Domain/Entity/Maker.php
+++ src/Domain/Entity/Maker.php
@@ -3,11 +3,19 @@
 namespace App\Domain\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use JMS\Serializer\Annotation as JMS;
 
+/**
+ * Class Maker
+ * @package App\Domain\Entity
+ * @JMS\ExclusionPolicy("all")
+ */
 class Maker extends AbstractEntity
 {
     /**
      * @var string
+     * @JMS\Expose()
+     * @JMS\Groups({"show_phone"})
      */
     protected $name;
 
