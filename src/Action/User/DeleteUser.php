f0951c3bd4b11260a38f791adc97c59da8d9449c
ludovicjj <jahanlud@gmail.com>
2019-03-22 03:53:45 +0100

    Update action delete with RequestResolver and Deleter. Return response with no data and status code 204

--- src/Action/User/DeleteUser.php
+++ src/Action/User/DeleteUser.php
@@ -2,15 +2,50 @@
 
 namespace App\Action\User;
 
+use App\Domain\User\DeleteUser\Deleter;
+use App\Domain\User\DeleteUser\DeleteUserInput;
+use App\Domain\User\DeleteUser\RequestResolver;
+use App\Responders\JsonResponder;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DeleteUser
 {
+    /** @var RequestResolver  */
+    protected $requestResolver;
+
+    /** @var Deleter  */
+    protected $deleter;
+
+    /**
+     * DeleteUser constructor.
+     * @param RequestResolver $requestResolver
+     * @param Deleter $deleter
+     */
+    public function __construct(
+        RequestResolver $requestResolver,
+        Deleter $deleter
+    ) {
+        $this->requestResolver = $requestResolver;
+        $this->deleter = $deleter;
+    }
+
     /**
      * @Route("/api/clients/{client_id}/users/{user_id}", name="delete_user", methods={"DELETE"})
+     * @param Request $request
+     * @return Response
+     * @throws \Doctrine\ORM\NonUniqueResultException
      */
-    public function delete()
+    public function delete(Request $request)
     {
+        /** @var DeleteUserInput $input */
+        $input = $this->requestResolver->resolve($request);
+        $this->deleter->delete($input);
 
+        return JsonResponder::response(
+            null,
+            Response::HTTP_NO_CONTENT
+        );
     }
 }
