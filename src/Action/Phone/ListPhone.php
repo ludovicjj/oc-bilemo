9e66dc8c83ebb787650232f4b90d62a8a181ffa6
ludovicjj <jahanlud@gmail.com>
2019-03-22 04:39:46 +0100

    Create action listPhone with Loader and NormalizerData. If data is not null return Response with status code 200 else 204

---
+++ src/Action/Phone/ListPhone.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Action\Phone;
+
+use App\Domain\Phone\ListPhone\Loader;
+use App\Domain\Phone\ListPhone\NormalizerData;
+use App\Responders\JsonResponder;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ListPhone
+{
+    /** @var Loader  */
+    protected $loader;
+
+    /** @var NormalizerData  */
+    protected $normalizerData;
+
+    /**
+     * ListPhone constructor.
+     * @param Loader $loader
+     * @param NormalizerData $normalizerData
+     */
+    public function __construct(
+        Loader $loader,
+        NormalizerData $normalizerData
+    ) {
+        $this->loader = $loader;
+        $this->normalizerData = $normalizerData;
+    }
+
+    /**
+     * @Route("/api/phones", name="list_phone", methods={"GET"})
+     * @return Response
+     */
+    public function listPhone()
+    {
+        $input = $this->loader->load();
+
+        $data = $this->normalizerData->normalize($input);
+
+        return JsonResponder::response(
+            $data,
+            is_null($data) ? Response::HTTP_NO_CONTENT : Response::HTTP_OK
+        );
+    }
+}
