002159435c66ffef0f0ad019a98bba2a5d053a76
ludovicjj <jahanlud@gmail.com>
2019-03-22 05:05:56 +0100

    Create method phoneExist in PhoneRepository

--- src/Domain/Repository/PhoneRepository.php
+++ src/Domain/Repository/PhoneRepository.php
@@ -12,4 +12,18 @@ class PhoneRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Phone::class);
     }
+
+    /**
+     * @param string $phoneId
+     * @return mixed
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function phoneExist(string $phoneId)
+    {
+        return $this->createQueryBuilder('p')
+            ->where('p.id = :phone_id')
+            ->setParameter('phone_id', $phoneId)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 }
