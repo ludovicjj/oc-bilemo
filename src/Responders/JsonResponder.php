0316278c30bcd70ec6a47dd33a7101638ecd5ccd
ludovicjj <jahanlud@gmail.com>
2019-03-23 15:49:05 +0100

    Add param cacheable=false in statis method response. If param cacheable is true, cache catching response for 3600 secondes

--- src/Responders/JsonResponder.php
+++ src/Responders/JsonResponder.php
@@ -9,12 +9,14 @@ class JsonResponder
     /**
      * @param string|null $data
      * @param int $statusCode
+     * @param bool $cacheable
      * @param array $additionalHeaders
      * @return Response
      */
     public static function response(
         ?string $data,
         int $statusCode = Response::HTTP_OK,
+        bool $cacheable = false,
         array $additionalHeaders = []
     ) {
         $response = new Response(
@@ -28,6 +30,13 @@ class JsonResponder
                 $additionalHeaders
             )
         );
+
+        if ($cacheable) {
+            $response
+                ->setPublic()
+                ->setSharedMaxAge(3600)
+                ->setMaxAge(3600);
+        }
 
         return $response;
     }
